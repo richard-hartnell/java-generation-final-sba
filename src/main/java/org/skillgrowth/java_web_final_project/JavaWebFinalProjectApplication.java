f859aaa9127e4512ff625657ac847b79438efe4b
unknown <richard@richardhartnell.com>
2023-06-08 18:31:11 -0700

    Added build

--- src/main/java/org/skillgrowth/java_web_final_project/JavaWebFinalProjectApplication.java
+++ src/main/java/org/skillgrowth/java_web_final_project/JavaWebFinalProjectApplication.java
@@ -2,8 +2,10 @@ package org.skillgrowth.java_web_final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+
 public class JavaWebFinalProjectApplication {
 
 	public static void main(String[] args) {
